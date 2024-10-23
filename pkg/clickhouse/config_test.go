c14745f079a25b5a4ab2450a1f22a261e05b5195
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2024-10-23 20:26:24 +0530

    feat: add support for Account and Region

--- pkg/clickhouse/config_test.go
+++ pkg/clickhouse/config_test.go
@@ -5,7 +5,8 @@ import (
 	"testing"
 	"time"
 
-	clickhouse "github.com/sid-maddy/xk6-output-clickhouse/pkg/clickhouse"
+	"github.com/sid-maddy/xk6-output-clickhouse/pkg/clickhouse"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"go.k6.io/k6/lib/types"
 	"go.k6.io/k6/output"
@@ -16,48 +17,75 @@ func TestConfig(t *testing.T) {
 
 	// TODO: add more cases
 	testCases := map[string]struct {
+		env map[string]string
+
+		arg string
+		err string
+
 		jsonRaw json.RawMessage
-		env     map[string]string
-		arg     string
 		config  clickhouse.Config
-		err     string
 	}{
 		"default": {
 			config: clickhouse.Config{
+				Table: "run_output",
+
 				PushInterval: types.NullDurationFrom(1 * time.Second),
+				LogLevel:     logrus.InfoLevel,
 			},
 		},
 
 		"overwrite": {
 			env: map[string]string{
 				"K6_CLICKHOUSE_DSN":           "clickhouse://user:pass@localhost:9000/k6",
-				"K6_CLICKHOUSE_RUN_ID":        "abc",
+				"K6_CLICKHOUSE_TABLE":         "k6_run_output",
+				"K6_CLICKHOUSE_ACCOUNT_ID":    "00000000-0000-0000-0000-000000000001",
+				"K6_CLICKHOUSE_REGION":        "asia-south1",
+				"K6_CLICKHOUSE_RUN_ID":        "00000000-0000-0000-0000-000000000002",
 				"K6_CLICKHOUSE_PUSH_INTERVAL": "4ms",
+				"K6_CLICKHOUSE_LOG_LEVEL":     "debug",
 			},
+
 			config: clickhouse.Config{
-				DSN:          "clickhouse://user:pass@localhost:9000/k6",
-				RunID:        "abc",
+				DSN:   "clickhouse://user:pass@localhost:9000/k6",
+				Table: "k6_run_output",
+
+				AccountID: "00000000-0000-0000-0000-000000000001",
+				Region:    "asia-south1",
+				RunID:     "00000000-0000-0000-0000-000000000002",
+
 				PushInterval: types.NullDurationFrom(4 * time.Millisecond),
+				LogLevel:     logrus.DebugLevel,
 			},
 		},
 
 		"early error": {
 			env: map[string]string{
 				"K6_CLICKHOUSE_DSN":           "clickhouse://user:pass@localhost:9000/k6",
+				"K6_CLICKHOUSE_TABLE":         "k6_run_output",
+				"K6_CLICKHOUSE_ACCOUNT_ID":    "00000000-0000-0000-0000-000000000001",
+				"K6_CLICKHOUSE_REGION":        "asia-south1",
 				"K6_CLICKHOUSE_RUN_ID":        "abc",
 				"K6_CLICKHOUSE_PUSH_INTERVAL": "4something",
+				"K6_CLICKHOUSE_LOG_LEVEL":     "debug",
 			},
-			config: clickhouse.Config{
-				DSN:          "clickhouse://user:pass@localhost:9000/k6",
-				RunID:        "abc",
-				PushInterval: types.NullDurationFrom(4 * time.Second),
-			},
+
 			err: `time: unknown unit "something" in duration "4something"`,
+
+			config: clickhouse.Config{
+				DSN:   "clickhouse://user:pass@localhost:9000/k6",
+				Table: "k6_run_output",
+
+				AccountID: "00000000-0000-0000-0000-000000000001",
+				Region:    "asia-south1",
+				RunID:     "00000000-0000-0000-0000-000000000002",
+
+				PushInterval: types.NullDurationFrom(4 * time.Second),
+				LogLevel:     logrus.DebugLevel,
+			},
 		},
 	}
 
 	for name, testCase := range testCases {
-		testCase := testCase
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
@@ -71,8 +99,16 @@ func TestConfig(t *testing.T) {
 			}
 
 			require.NoError(t, err)
+
 			require.Equal(t, testCase.config.DSN, config.DSN)
+			require.Equal(t, testCase.config.Table, config.Table)
+
+			require.Equal(t, testCase.config.AccountID, config.AccountID)
+			require.Equal(t, testCase.config.Region, config.Region)
+			require.Equal(t, testCase.config.RunID, config.RunID)
+
 			require.Equal(t, testCase.config.PushInterval, config.PushInterval)
+			require.Equal(t, testCase.config.LogLevel, config.LogLevel)
 		})
 	}
 }
